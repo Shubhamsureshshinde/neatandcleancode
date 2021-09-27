cf65a69cca26e3df641e575550e2af4a0cd0468c
Shubham Shinde <shindeshubham623@gmail.com>
2021-09-27 22:50:09 +0530

    first commit

---
+++ functions.js
@@ -0,0 +1,52 @@
+// const Name = "shubham";
+// const occupation = "developer";
+// const collage = "aissms pune";
+// const birthyear = 1997;
+// const now = 2021;
+
+// const introduction = "my name is "+  Name + " and i'm " +  
+// occupation +" studied from " + collage + " i'm "+
+//  (now-birthyear)+ " years old";
+// console.log(introduction);
+
+// const introductionnew = `my name is ${Name},and i'm
+//  ${occupation},studied from ${collage}, i'm ${now-birthyear},years old`;
+//  console.log(introductionnew);
+
+//  console.log (`just a regular theme`);
+
+//  console.log(`my heart is stereo\n\
+//  beats for you \n\ listen close`)
+
+
+//  const age = 15;
+//  const requiredage = age>=18;
+
+//  if (requiredage) {
+//      console.log('i can drive the car');
+//  }else {
+//     const yearsleft = 18-age ;
+//     console.log("after "+yearsleft+" years i can drive the car")
+//  }
+ 
+
+// const value = 250;
+// const requiredvalue = value>=400;
+
+// if (requiredvalue) {
+//     console.log("i can buy things");
+// }else {
+//     truevalue = 400-value
+//     console.log("need "+truevalue+ " more")
+
+
+const value = 800;
+const requiredvalue = value>=1000;
+
+if (requiredvalue)  {
+    console.log("we can buy things");
+}else
+{truevalue = 1000-value
+    console.log("need "+truevalue+ " more")
+
+}
